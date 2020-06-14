b14b3979ae575179fed7850ad3e394b411d4cbc3
jasvalle <54610752+jasvalle@users.noreply.github.com>
2020-06-13 19:32:25 -0700

    final

--- Final/script.js
+++ Final/script.js
@@ -1,20 +1,28 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    if($(this).scrollTop() < $("#green").height()){
-       $(".navbar").removeClass("bg-dark");
-    }
-    else{
-       $(".navbar").addClass("bg-dark");
-    }
-  });
+$(document).ready(function () {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() < $("#green").height()) {
+         $(".navbar").removeClass("bg-dark");
+      } else {
+         $(".navbar").addClass("bg-dark");
+      }
+   });
 
-  $(window).scroll(function() {
-    if($(this).scrollTop() < $("#green").height()){
-       $(".dropdown-menu").removeClass("dropdowncolor");
-    }
-    else{
-       $(".dropdown-menu").addClass("dropdowncolor");
-    }
-  });
+   $(window).scroll(function () {
+      if ($(this).scrollTop() < $("#green").height()) {
+         $(".dropdown-menu").removeClass("dropdowncolor");
+      } else {
+         $(".dropdown-menu").addClass("dropdowncolor");
+      }
+   });
+
+   $('#target').click(function () {
+      $('#output').html(function (i, val) {
+         return val * 1 + 1
+      });
+   });
+
+   AOS.init({
+      duration: 1200,
+   })
+   $('.carousel').carousel()
 });
-
